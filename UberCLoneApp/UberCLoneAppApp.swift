105c5c678f7fe116ae151e68552ed48b4d52ae0d
lucas-newlands <lucas.newlands@ibm.com>
2023-11-23 14:29:21 -0300

    Initial Commit

---
+++ UberCLoneApp/UberCLoneAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberCLoneAppApp.swift
+//  UberCLoneApp
+//
+//  Created by Lucas Newlands on 23/11/23.
+//
+
+import SwiftUI
+
+@main
+struct UberCLoneAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
